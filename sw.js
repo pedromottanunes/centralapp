c200c42f3f9dfb2d6e3d71a77751734330276db0
pedromottanunes <pedromottanunes@gmail.com>
2025-06-06 10:24:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,3 @@ self.addEventListener('install', function(e) {
   console.log('Service Worker instalado');
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
-});
